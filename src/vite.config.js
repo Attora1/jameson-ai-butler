3380797ec19f6c7ab44c088f3938c62b2c12f21a
Attora1 <vmukes96@email.com>
2025-08-17 14:17:44 -0400

    chore: update vite proxy to use port 5000

--- src/vite.config.js
+++ src/vite.config.js
@@ -44,12 +44,8 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+      '/api': 'http://localhost:5000',
+    },
   },
   build: {
     sourcemap: true,
